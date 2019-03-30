1ddc3f21542eb1e2e87c731e97e1f1f556b3d230
yucheng <yucheng.zhu@shuyun.com>
2019-03-30 17:31:23 +0800

    project init

---
+++ topic/src/main/java/com/example/topic/TopicApplication.java
@@ -0,0 +1,16 @@
+package com.example.topic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhuyucheng
+ */
+@SpringBootApplication
+public class TopicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopicApplication.class, args);
+    }
+
+}
